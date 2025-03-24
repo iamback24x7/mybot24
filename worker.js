99c18040e84b0b22a66d785d06992aac6f9e45a6
iamback24x7 <107175697+iamback24x7@users.noreply.github.com>
2025-03-24 21:19:56 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "09814685583:AAH_nJs7-OKiFuHkYedfhrQV5LjkCB5g"; // Insert your bot token.
+const BOT_TOKEN = "7853019382:AAHgv6smnU5an-God1bLqFjNnljJLGv-ouI"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text.
+const BOT_SECRET = "prathyoush"; // Insert a powerful secret text.
 const BOT_OWNER = 0064350805; // Insert your telegram account id.
 const BOT_CHANNEL = -10020358474134; // Insert your telegram channel id which the bot is admin in.
 const SIA_NUMBER = 3812; // Insert a random integer number and keep it safe.
